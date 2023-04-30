9668a5ea01ac8556fbf065f236fea88960e1e2a3
Siarhei.Kharlap <s.kharlap@dealcoud.com>
2023-04-30 15:49:38 +0200

    Add init project

---
+++ src/OpenIdDictSample/OpenIdDictSample/ClientSeeder.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OpenIddict.Abstractions;
+
+namespace OpenIdDictSample
+{
+    public class ClientSeeder : IHostedService
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public ClientSeeder(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            using var scope = _serviceProvider.CreateScope();
+
+            var context = scope.ServiceProvider.GetRequiredService<DbContext>();
+
+            await PopulateScopes(scope, cancellationToken);
+
+            await PopulateInternalApps(scope, cancellationToken);
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+
+        private async Task PopulateScopes(IServiceScope scope, CancellationToken cancellationToken)
+        {
+            var scopeManager = scope.ServiceProvider.GetRequiredService<IOpenIddictScopeManager>();
+
+            var scopeDescriptor = new OpenIddictScopeDescriptor
+            {
+                Name = "test_scope",
+                Resources = { "test_resource" }
+            };
+
+            var scopeInstance = await scopeManager.FindByNameAsync(scopeDescriptor.Name, cancellationToken);
+
+            if (scopeInstance == null)
+            {
+                await scopeManager.CreateAsync(scopeDescriptor, cancellationToken);
+            }
+            else
+            {
+                await scopeManager.UpdateAsync(scopeInstance, scopeDescriptor, cancellationToken);
+            }
+        }
+
+        private async Task PopulateInternalApps(IServiceScope scopeService, CancellationToken cancellationToken)
+        {
+            var appManager = scopeService.ServiceProvider.GetRequiredService<IOpenIddictApplicationManager>();
+
+            var appDescriptor = new OpenIddictApplicationDescriptor
+            {
+                ClientId = "test_client",
+                ClientSecret = "test_secret",
+                Type = OpenIddictConstants.ClientTypes.Confidential,
+                Permissions =
+                    {
+                        OpenIddictConstants.Permissions.Endpoints.Token,
+                        OpenIddictConstants.Permissions.Endpoints.Introspection,
+                        OpenIddictConstants.Permissions.Endpoints.Revocation,
+
+                        OpenIddictConstants.Permissions.GrantTypes.ClientCredentials,
+                        OpenIddictConstants.Permissions.GrantTypes.RefreshToken,
+
+                        OpenIddictConstants.Permissions.Prefixes.Scope + "test_scope"
+                    }
+            };
+
+            var client = await appManager.FindByClientIdAsync(appDescriptor.ClientId, cancellationToken);
+
+            if (client == null)
+            {
+                await appManager.CreateAsync(appDescriptor, cancellationToken);
+            }
+            else
+            {
+                await appManager.UpdateAsync(client, appDescriptor, cancellationToken);
+            }
+        }
+    }
+}
+
---
+++ src/OpenIdDictSample/OpenIdDictSample/Program.cs
@@ -0,0 +1,66 @@
+using System.Security.Cryptography.X509Certificates;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using OpenIdDictSample;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddDbContext<DbContext>(
+    options =>
+    {
+        options.UseInMemoryDatabase(nameof(DbContext));
+        options.UseOpenIddict();
+    }
+    );
+
+builder.Services.AddOpenIddict()
+    .AddCore(
+    _ =>
+    {
+        _.UseEntityFrameworkCore()
+            .UseDbContext<DbContext>();
+        ////.ReplaceWithDcOAuthEntities()
+        //    .UseDbContext<DbContext>();
+    }
+    )
+    .AddServer(
+    _ =>
+    {
+        _.AllowClientCredentialsFlow();
+        //.AllowPasswordFlow()
+        //.AllowRefreshTokenFlow();
+
+        _.SetTokenEndpointUris("token");
+        //       .SetIntrospectionEndpointUris(serverConfiguration.IntrospectEndpoint)
+        //       .SetRevocationEndpointUris(serverConfiguration.RevokeEndpoint);
+
+        _.AddDevelopmentEncryptionCertificate()
+         .AddDevelopmentSigningCertificate();
+
+        //disable access token payload encryption
+        _.DisableAccessTokenEncryption();
+
+        _.UseAspNetCore().EnableTokenEndpointPassthrough();
+
+        //_.AddEventHandler(new Open)
+    }
+    );
+
+builder.Services.AddHostedService<ClientSeeder>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+//app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
